8d52dc9b7ca6f83b382309ab2b93e7288e0b37a6
szabomarcel <szabomarcell34@gmail.com>
2023-09-15 21:41:44 +0200

    vizsga

--- jegyvasarlas.php
+++ jegyvasarlas.php
@@ -42,9 +42,9 @@
     </nav>
 
     <!-- Bootstrap JavaScript fájl importálása (opcionális, ha használunk dropdown menüket vagy más Bootstrap komponenseket) -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <!--<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>-->
 
     <div class="container-fluid">
         <div class="gomb">
