51180a2c41aa0c4cacfd9d1b95c44fd8b60248cc
szabomarcel <szabomarcell34@gmail.com>
2023-09-25 18:16:27 +0200

    vizsga

---
+++ menu.php
@@ -0,0 +1,34 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="fooldal.html">Főoldal</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="fooldal.html">Főoldal</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="regisztracio.html">Jegyvásárlás</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="atigazolasi hirek.html">Átigazolási hírek</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Napi kwiz</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Mérkőzések</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="forraskodok.html">Forrás kódok</a>
+            </li>
+        </ul>
+        <ul class="nav-item">
+            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        </ul>
+        <ul class="nav-item">
+            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+        </ul>
+    </div>
+</nav>
