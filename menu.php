73ac88391801688bb000b87952da59a0fd4efa2b
szabomarcel <szabomarcell34@gmail.com>
2023-09-28 14:10:12 +0200

    vizsga

--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="fooldal.html">Főoldal</a>
+    <a class="navbar-brand" href="index.php?menuItem=fooldal">Főoldal</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
@@ -15,10 +15,13 @@
                 <a class="nav-link" href="index.php?menuItem=atigazolas">Átigazolási hírek</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Napi kwiz</a>
+                <a class="nav-link" href="index.php?menuItem=kwiz">Napi kwiz</a>
             </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Mérkőzések</a>
+                <a class="nav-link" href="index.php?menuItem=csapat">Csapatok</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="index.php?menuItem=foci">Mini Játék focival kapcsolatos</a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="index.php?menuItem=forras">Forrás kódok</a>
