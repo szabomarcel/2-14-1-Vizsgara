8ef114146b79535785c6f886d0755d5de8933f79
szabomarcel <szabomarcell34@gmail.com>
2023-10-19 18:42:54 +0200

    vizsga

--- Zaro_Vizsga/login/regisztracio.php
+++ Zaro_Vizsga/login/regisztracio.php
@@ -86,10 +86,10 @@
     <button type="submit" class="btn btn-primary" name="regisztraciosAdatok" value="true">Regisztáció</button>
   </form>
 </div>
+</form>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-</form>
 <hr>
 <main>
     <h2>Sponzorok</h2>
