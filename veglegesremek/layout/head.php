fc0862ebebab2b4565f21a64bb0d725caf932ed7
szabomarcel <szabomarcell34@gmail.com>
2023-11-21 21:09:25 +0100

    remek

--- veglegesremek/layout/head.php
+++ veglegesremek/layout/head.php
@@ -10,7 +10,13 @@
     <link href="css/vizsga2.css" rel="stylesheet">
     <link href="css/login.css" rel="stylesheet">
     <link href="css/register.css" rel="stylesheet">
-    <link href="css/main.css" rel="stylesheet">
+    <link href="assets/css/variables.css" rel="stylesheet">
+    <link href="assets/css/main.css" rel="stylesheet">
+    <!-- Vendor CSS Files -->
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <!--<link href="assets/vendor/aos/aos.css" rel="stylesheet">-->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="apple-touch-icon" sizes="180x180" href="kepek/foci/pele-focilabda.jpg">
     <link rel="icon" type="image/png" sizes="32x32" href="kepek/foci/pele-focilabda.jpg">
