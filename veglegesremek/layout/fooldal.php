1864a959ad44cf96d3932b8c822e664f2b3f3a3e
szabomarcel <szabomarcell34@gmail.com>
2023-11-30 20:54:59 +0100

    remek

--- veglegesremek/layout/fooldal.php
+++ veglegesremek/layout/fooldal.php
@@ -371,85 +371,54 @@
   </section>
   <section id="contact" class="contact">
       <div class="container" data-aos="fade-up">
-
         <div class="section-header">
           <h2>Kontaktok</h2>
           <p>Miben tudunk segíteni? <span>Contact Us</span></p>
         </div>
-
         <div class="mb-3">
           <iframe style="border:0; width: 100%; height: 350px;" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" allowfullscreen></iframe>
         </div><!-- End Google Maps -->
-
         <div class="row gy-4">
-
           <div class="col-md-6">
             <div class="info-item  d-flex align-items-center">
-              <i class="icon bi bi-map flex-shrink-0"></i>
               <div>
                 <h3>Helyszín</h3>
                 <p>Burgunia utca, Debrecen, 4023</p>
+                <i class="icon bi bi-map flex-shrink-0"></i>
               </div>
             </div>
           </div><!-- End Info Item -->
-
           <div class="col-md-6">
             <div class="info-item d-flex align-items-center">
-              <i class="icon bi bi-envelope flex-shrink-0"></i>
-              <div>
-                <h3>Email Cím:</h3>
+            <div>
+              <h3>Email Cím:</h3>
                 <p>burgundia34@gmail.com</p>
+                <i class="icon bi bi-envelope flex-shrink-0"></i>
               </div>
             </div>
           </div><!-- End Info Item -->
-
           <div class="col-md-6">
             <div class="info-item  d-flex align-items-center">
-              <i class="icon bi bi-telephone flex-shrink-0"></i>
               <div>
                 <h3>Telefonszám:</h3>
                 <p>+70 20 999 9999</p>
+                <i class="icon bi bi-telephone flex-shrink-0"></i>
               </div>
             </div>
           </div><!-- End Info Item -->
-
           <div class="col-md-6">
             <div class="info-item  d-flex align-items-center">
-              <i class="icon bi bi-share flex-shrink-0"></i>
               <div>
                 <h3>Nytva tartás</h3>
-                <div><strong>Hétfőtől - Péntekig:</strong> 8AM - 15PM;
+                <div>
+                  <strong>Hétfőtől - Péntekig:</strong> 8AM - 15PM; <br>
                   <strong>Szombat és vasárnap:</strong> Zárva
                 </div>
+                <i class="icon bi bi-share flex-shrink-0"></i>
               </div>
             </div>
           </div><!-- End Info Item -->
-
         </div>
-
-        <!--<form action="forms/contact.php" method="post" role="form" class="php-email-form p-3 p-md-4">
-          <div class="row">
-            <div class="col-xl-6 form-group">
-              <input type="text" name="name" class="form-control" id="name" placeholder="Your Name" required>
-            </div>
-            <div class="col-xl-6 form-group">
-              <input type="email" class="form-control" name="email" id="email" placeholder="Your Email" required>
-            </div>
-          </div>
-          <div class="form-group">
-            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject" required>
-          </div>
-          <div class="form-group">
-            <textarea class="form-control" name="message" rows="5" placeholder="Message" required></textarea>
-          </div>
-          <div class="my-3">
-            <div class="loading">Loading</div>
-            <div class="error-message"></div>
-            <div class="sent-message">Your message has been sent. Thank you!</div>
-          </div>
-          <div class="text-center"><button type="submit">Send Message</button></div>
-        </form><!--End Contact Form -->
-
       </div>
     </section><!-- End Contact Section -->
 </main>
